24ba5a4f586d45d79bdaca181cf01a96c3fc418a
bekjonalijonov <bekjon200101@gmail.com>
2025-11-23 22:04:05 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,10 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost/habit")
+DATABASE_URL = os.getenv("DATABASE_URL")  # Railway avto beradi
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+else:
+    DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost/habit"
 TELEGRAPH_URL = os.getenv("TELEGRAPH_URL")
 START_DATE = "2025-11-24"  # o‘zgartirishingiz mumkin
